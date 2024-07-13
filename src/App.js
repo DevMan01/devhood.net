eea3497e08c0fe4eb35f754430ffd88ce2539bd2
Tim Chaffin <tim.chaffin@icloud.com>
2024-07-13 12:41:27 -0600

    Smaller Box, Floating above background.

--- src/App.js
+++ src/App.js
@@ -17,15 +17,19 @@ function App() {
 
   // Define the Style for the Box
   const floatingBoxStyle = {
-    backgroundColor: 'rgba(64, 64, 64, 0.5)', // Grey with semi-transparency
+    backgroundColor: 'rgba(64, 64, 64, 0.8)', // Grey with semi-transparency
     borderRadius: '5vw', // Rounded corners
     padding: '2vw', // Padding inside the box
-    width: '80vw', // Width of the box
+    width: '40vw', // Width of the box
     height: '50vh', // Height of the box
     display: 'flex', // To center content inside the box, if needed
     justifyContent: 'center', // Center horizontally inside the box
     alignItems: 'center', // Center vertically inside the box
     boxShadow: '1vw 1vw 3vw rgba(0, 0, 0, 0.5)', // Responsive Shadow to the bottom right
+    position: 'fixed', // Keeps the box fixed during scrolling
+    top: '50%', // Centers the box vertically
+    left: '50%', // Centers the box horizontally
+    transform: 'translate(-50%, -50%)', // Adjusts the box to be centered correctly
   };
 
   return (
