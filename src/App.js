6f0835f8dfddad5d45c6798a23e44ce3f1def83c
Tim Chaffin <tim.chaffin@icloud.com>
2024-07-13 01:49:07 -0600

    Okay, what about a sweet background?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 function App() {
-  const value = 'Jen';
-  return <div>Hey {value}, thinking of you. I wish we were cuddling.</div>;
+  
+  // Building a constant to hold the background style
+  const backgroundStyle = {
+    backgroundImage: 'url(/homepage.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100vw'
+  };
+
+  return (
+    <div style={backgroundStyle}>
+      <div>Hey Jen, thinking of you. I wish we were cuddling.</div>
+    </div>
+  );
 }
 
 export default App;
